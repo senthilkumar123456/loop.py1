744e878bd10f842fad5c14606a67cdd91f2db6b7
senthilkumar123456 <141622183+senthilkumar123456@users.noreply.github.com>
2023-08-25 12:51:59 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,45 @@
+##rows=int(input('ente the value'))
+##for i in range(rows,0,-1):
+##    for j in range(0,i,1):
+##        print('*',end='')
+##    print()
+##task 5
+##rows=int(input('enter the rows value'))
+##cols=int(input('enter the cols value'))
+##a=65
+##for i in range(1,rows+1):
+##    for s in range(1,cols+1):
+##        print(chr(a),end=' ')
+##        a+=1
+##        if(a==68):
+##            a-=3
+##            print()
+##task 6
+##rows=int(input('enter the rows value'))
+##cols=int(input('enter the cols value'))
+##a=64
+##for i in range(1, rows+1):
+##    a+=1
+##    for j in range(1,cols+1):
+##        print(chr(a),end=' ')
+##
+##        
+##
+##    print()
+## task 6
+##rows=int(input('enter the value'))
+##cols=int(input('enter the value'))
+##a=64
+##for i in range(1,rows+1):
+##    for i in range(1,cols+1):
+##        a+=1
+##        print(chr(a),end=' ')
+##    print()
+##task 7
+rows=int(input('enter the value'))
+for i in range(1,rows+1):
+    for j in range(0,i):
+        print(i,end=' ')
+    print()
+
+
